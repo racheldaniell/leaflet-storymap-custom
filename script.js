cd7eb3d4be32003cf9f01e5a768cf4d92d728f47
racheldaniell <44450960+racheldaniell@users.noreply.github.com>
2021-04-14 17:19:42 -0400

    Add files via upload
    basemup update

--- script.js
+++ script.js
@@ -18,7 +18,11 @@ function initMap() {
   // This displays a base layer map (other options available)
   var lightAll = new L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
     attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map);
+  ,
+  subdomains: 'abcd',
+  minZoom: 1,
+  maxZoom: 20,
+  ext: 'jpg'}).addTo(map);
 
   // This customizes link to view source code; add your own GitHub repository
   map.attributionControl
