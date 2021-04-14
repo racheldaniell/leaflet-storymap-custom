90ab55dfd9b610fc9b0d8c7a25a52f8caf429f61
racheldaniell <44450960+racheldaniell@users.noreply.github.com>
2021-04-14 17:24:38 -0400

    Add files via upload
    changed to terrain map

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function initMap() {
   });
 
   // This displays a base layer map (other options available)
-  var lightAll = new L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+  var lightAll = new L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}', {
     attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   ,
   subdomains: 'abcd',
