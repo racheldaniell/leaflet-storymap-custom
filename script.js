43462e35109e65c6787cb9566fcf4bfbe4b9045b
racheldaniell <44450960+racheldaniell@users.noreply.github.com>
2021-04-14 17:16:26 -0400

    Add files via upload
    has markers color change and basemap change

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ function initMap() {
   });
 
   // This displays a base layer map (other options available)
-  var lightAll = new L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+  var lightAll = new L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
 
   // This customizes link to view source code; add your own GitHub repository
